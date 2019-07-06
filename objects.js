0f9979e70c647e97b2f459a2fa61ea30062fa18e
Felix Martinez <felixaviermartinez@gmail.com>
2019-07-06 16:20:28 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ var playlist = {
 };
 
 function updatePlaylist(obj, key, value) {
-
+  return Object.assign({}, playlist, {'Phil Ochs': ["Here's to the State of Mississippi"]});
 }
 
 function removeFromPlaylist(obj, key) {
